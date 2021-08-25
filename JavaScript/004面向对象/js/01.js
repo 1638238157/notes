b263b714871d3f5480a4833364e0239a63f51191
陈浩然 <1638238157@qq.com>
2021-08-25 15:25:11 +0800

    介绍对象,Object.is 方法

--- JavaScript/004面向对象/js/01.js
+++ JavaScript/004面向对象/js/01.js
@@ -75,28 +75,118 @@
 // console.log(book.edition);
 
 // 在一个对象上同时定义多个属性  Object.defineProperties 两个参数  第一个 添加或修改属性的对象  第二个 另一个描述对象
-let book = {};
-Object.defineProperties(book,{
-    year_: {
-        value: 2017
-    },
-    edition:{
-        value: 1
-    },
-    year:{
-        get(){
-            return this.year_;
-        },
-        set(newValue){
-            if(newValue > 2017){
-                this.year_ = newValue;
-                this.edition += newValue - 2017;
-            }
-        }
-    }
-});
+// let book = {};
+// Object.defineProperties(book,{
+//     year_: {
+//         value: 2017
+//     },
+//     edition:{
+//         value: 1
+//     },
+//     year:{
+//         get(){
+//             return this.year_;
+//         },
+//         set(newValue){
+//             if(newValue > 2017){
+//                 this.year_ = newValue;
+//                 this.edition += newValue - 2017;
+//             }
+//         }
+//     }
+// });
 // 获取指定属性的描述符。两个参数  第一个  属性所在的对象  第二个 要获取描述符的属性名
 // let descriptor = Object.getOwnPropertyDescriptor(book,"year_");
 // console.log(descriptor);
-let descriptor = Object.getOwnPropertyDescriptor(book,"year");
-console.log(descriptor);
+// let descriptor = Object.getOwnPropertyDescriptor(book,"year");
+// console.log(descriptor);
+
+// Object.getOwnPropertyDescriptors  会在每个自有属性上调用 Object.getOwnPropertyDescriptor 并在一个新对象中返回他们
+// let descriptorsResult = Object.getOwnPropertyDescriptors(book);
+// console.log(descriptorsResult);
+
+// let person = {
+//     name: 'kyrie'
+// }
+// let personDescriptor = Object.getOwnPropertyDescriptor(person, "name");
+// console.log(personDescriptor);
+
+// 合并对象
+// 简单复制
+/*
+    Object.assign() 修改目标对象 返回修改后的目标对象 (浅复制)
+    该方法接受一个目标对象和一个或多个源对象作为参数，将每个源对象中可枚举和自有属性复制到目标对象。
+    多个源对象都有相同的属性，则使用最后一个复制的值。
+*/
+let dest, src, result;
+dest = {};
+src = {id: 'src'};
+result = Object.assign(dest, src);
+console.log(dest === result);
+console.log(dest !== src);
+console.log(result);
+console.log(dest);
+
+/**
+ * 多个源对象
+ */
+dest = {};
+result = Object.assign(dest, {a:'foo'},{b: 'bar'});
+console.log(result);
+
+/**
+ * 获取函数与设置函数
+ */
+dest = {
+    set a(val){
+        console.log(`Invoked dest setter with param ${val}`);
+    }
+};
+src = {
+    get a(){
+        console.log('Invoked src getter');
+        return 'foo';
+    }
+}
+Object.assign(dest, src);
+// 调用 src 的获取方法
+// 调用 dest 的设置方法并传入参数"foo"
+// 因为这里的设置函数不执行赋值操作
+// 所以实际上并没有把值转移过来
+console.log(dest);
+
+// 覆盖属性
+
+dest = {id: 'dest'};
+result = Object.assign(dest,{id: 'src1'},{id: 'src2'});
+console.log(result); // {id: "src2"}
+// 可以通过目标对象上的设置函数观察到覆盖的过程:
+ dest = {
+    set id(x) {
+        console.log(x);
+} };
+Object.assign(dest, { id: 'first' }, { id: 'second' }, { id: 'third' });
+// first
+// second
+// third
+
+/**
+* 对象引用 */
+dest = {};
+src = { a: {} };
+Object.assign(dest, src);
+// 浅复制意味着只会复制对象的引用 
+console.log(dest); // {a:{}}
+console.log(dest.a === src.a); // true
+
+//嵌套结构
+// let person = {
+//     name: 'Matt',
+//     age: 27,
+//     job: {
+//         title: 'Software engineer'
+//     }
+// };
+
+// const {job: {title},job: personJob} = person;
+// console.log(title,personJob);
